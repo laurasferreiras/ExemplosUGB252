c736e0d6b9271109aa4956a493ed9d1d13de8625
2024101324 <2024101324@LAB1-014>
2025-08-12 19:23:08 -0300

    Adicionar arquivos de projeto.

---
+++ ConceitosBasicos/Program.cs
@@ -0,0 +1,10 @@
+﻿int idade = 18;
+Console.WriteLine("A idade é " + idade);
+if (idade >= 18)
+{
+    Console.WriteLine("vc pode tirar habilitação");
+}
+else
+{
+    Console.WriteLine("vc n pode tirar");
+}
