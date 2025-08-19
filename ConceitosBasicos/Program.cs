d2378d725ceea960635e3082d75fe52e58cb7e3d
laurasferreiras <laurasferreira6@gmail.com>
2025-08-19 20:10:03 -0300

    try and catch

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -1,37 +1,54 @@
-﻿Console.WriteLine("Informe seu nome: ");
-string nome = Console.ReadLine();
-Console.WriteLine("Informe seu salário: ");
-decimal salario = decimal.Parse (Console.ReadLine());
-
-// Console.WriteLine("Bem-vinda {0}, o salário de {1}", nome, salarioStr);
-Console.WriteLine($"Bem-vinda {nome}, seu salário é de {salario:C2}");
-
-// informe o valor do IRPF a ser pago de acordo com o salário informado. utilize a tabela real de desconto de IRPF
-
-if (salario <= 2259.20m)
+﻿using System.Linq.Expressions;
+try
 {
-    Console.WriteLine($"Você está isento de taxa");
+    Console.WriteLine("Informe seu nome: ");
+    string nome = Console.ReadLine();
+    Console.WriteLine("Informe seu salário: ");
+    decimal salario = decimal.Parse(Console.ReadLine());
+    decimal novoSalario = decimal.Parse(Console.ReadLine());
+
+    // Console.WriteLine("Bem-vinda {0}, o salário de {1}", nome, salarioStr);
+    Console.WriteLine($"Bem-vinda {nome}, seu salário é de {salario:C2}");
+
+    // informe o valor do IRPF a ser pago de acordo com o salário informado. utilize a tabela real de desconto de IRPF
+
+    if (salario <= 2259.20m)
+    {
+        Console.WriteLine($"Você está isento de taxa");
+    }
+    else if (salario >= 2259.21m && salario <= 2826.65m)
+    {
+         novoSalario = salario - (salario * 0.075m - 169.21m);
+        Console.WriteLine($"Seu sálário com o desconto é de {novoSalario:C2}");
+    }
+
+    else if (salario >= 2826.66m && salario <= 3751.05m)
+    {
+         novoSalario = salario - (salario * 0.15m - 381.44m);
+        Console.WriteLine($"Seu salário com o desconto é de {novoSalario:C2}");
+    }
+
+    else if (salario >= 3751.06m && salario <= 4664.68m)
+    {
+         novoSalario = salario - (salario * 0.225m - 662.67m);
+        Console.WriteLine($"Seu salário com o desconto é de {novoSalario:C2}");
+    }
+
+    else
+    {
+         novoSalario = salario - (salario * 0.275m - 896m);
+        Console.WriteLine($"Seu salário com o desconto é de {novoSalario:C2}");
+    }
+
 }
-else if  (salario >= 2259.21m &&  salario <= 2826.65m)
+catch (FormatException) //erro específico
 {
-    decimal novoSalario = salario - (salario * 0.075m - 169.21m);
-    Console.WriteLine($"Seu sálário com o desconto é de {novoSalario:C2}");
+    Console.WriteLine("O salário deve ser informado em formato numérico.");
 }
 
-else if (salario >= 2826.66m && salario <= 3751.05m)
+catch (Exception e) //qualquer tipo de erro
 {
-    decimal novoSalario = salario - (salario * 0.15m-381.44m);
-    Console.WriteLine($"Seu salário com o desconto é de {novoSalario:C2}");
+    Console.WriteLine("Ocorreu um erro. Informe ao suporte técnico");
+    Console.WriteLine(e.StackTrace);
 }
 
-else if (salario >= 3751.06m && salario <= 4664.68m)
-{
-    decimal novoSalario = salario - (salario * 0.225m- 662.67m);
-    Console.WriteLine($"Seu salário com o desconto é de {novoSalario:C2}");
-}
-
-else
-{
-    decimal novoSalario = salario - (salario * 0.275m - 896m);
-    Console.WriteLine($"Seu salário com o desconto é de {novoSalario:C2}");
-}
