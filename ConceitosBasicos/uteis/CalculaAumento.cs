d5e41c5a8a09340c13d052a0edc0a2b1a3632af9
laurasferreiras <laurasferreira6@gmail.com>
2025-08-26 20:27:46 -0300

    menu completo

---
+++ ConceitosBasicos/uteis/CalculaAumento.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosBasicos.uteis
+{
+    public class CalculaAumento
+    {
+        public decimal Calcular(decimal salario, float aumento)
+        {
+            /*
+            return salario + (salario * (decimal)aumento) / 100;
+            */
+            return salario * ((decimal)aumento / 100);
+        }
+    }
+}
---
+++ ConceitosBasicos/uteis/CalculaDesconto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceitosBasicos.uteis
+{
+    public class CalculaDesconto
+    {
+        public decimal Descontar(decimal valor, float desconto)
+        {
+            return valor * ((decimal)desconto / 100);
+        }
+    }
+}
