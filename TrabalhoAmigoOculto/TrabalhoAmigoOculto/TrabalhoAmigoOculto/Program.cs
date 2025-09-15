4310c34da562fb45f8f2cd0979e833f3f6f65a02
laurasferreiras <laurasferreira6@gmail.com>
2025-09-14 21:00:12 -0300

    amigo oculto

---
+++ TrabalhoAmigoOculto/TrabalhoAmigoOculto/TrabalhoAmigoOculto/Program.cs
@@ -0,0 +1,18 @@
+﻿using System; //importa tudo necessário para o programa rodar
+using System.Collections.Generic; //importa as listas, dicionários
+using System.Linq; //consulta em coleções de dados (listas, dict)
+
+//namespace AmigoOculto;
+//class Principal
+//{
+//    private static List<string> pessoas = new List<string>(); //aqui ele vai receber as pessoas do sorteio
+//    private static Dictionary<string, string> duplasSorteadas = new Dictionary<string, string>(); //recebe as duplas sorteadas
+//    private static bool sorteioRealizado = false;
+
+//    static void Main(string[] args) //método não retorna, só executa métodos
+//    {
+
+//    }
+
+//}
+
---
+++ TrabalhoAmigoOculto/TrabalhoAmigoOculto/TrabalhoAmigoOculto/classes/GerenciadorPessoas.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabalhoAmigoOculto.classes
+{
+    public class GerenciadorPessoas
+    {
+        public List<string> pessoas = new List<string>();
+        public bool AdicionarPessoa(string nome)
+        {
+            if (string.IsNullOrEmpty(nome))
+            {
+                Console.WriteLine("Nome inválido");
+                return false;
+            }
+
+            if (pessoas.Contains(nome, StringComparer.OrdinalIgnoreCase))
+            {
+                Console.WriteLine("Nome já cadastrado");
+                return false;
+            }
+
+            pessoas.Add(nome);
+            Console.WriteLine($"Pessoa {nome} cadastrada com sucesso!");
+            return true;
+
+        }
+        public List<string> ObterPessoas()
+        {
+            return new List<string>(pessoas);
+        }
+
+        public void ExibirPessoas()
+        {
+            if (pessoas.Count == 0)
+            {
+                Console.WriteLine("Nenhuma pessoa cadastrada.");
+                return;
+            }
+            Console.WriteLine("Pessoas cadastradas:");
+            foreach (var pessoa in pessoas)
+            {
+                Console.WriteLine($"- {pessoa}");
+            }
+
+
+        }
+    }
+}
